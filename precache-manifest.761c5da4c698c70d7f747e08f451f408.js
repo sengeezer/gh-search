481926b15104599e422b7b50a4c22b0a500e2f01
Fabio Albertin <albertin@gmail.com>
2019-11-01 14:22:23 +0000

    Updates

---
+++ precache-manifest.761c5da4c698c70d7f747e08f451f408.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38e035faab9dc6b2f898410052fa7cb",
+    "url": "/gh-search/index.html"
+  },
+  {
+    "revision": "a60947bd622fc2b8591a",
+    "url": "/gh-search/static/css/main.7173a3c8.chunk.css"
+  },
+  {
+    "revision": "f824da2479d1f155e180",
+    "url": "/gh-search/static/js/2.e705365d.chunk.js"
+  },
+  {
+    "revision": "a60947bd622fc2b8591a",
+    "url": "/gh-search/static/js/main.85101e4b.chunk.js"
+  },
+  {
+    "revision": "c9ffed04720f661f144d",
+    "url": "/gh-search/static/js/runtime-main.98cd55b0.js"
+  }
+]);
